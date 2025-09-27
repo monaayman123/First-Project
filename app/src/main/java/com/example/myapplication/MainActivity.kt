90f18158a7b27c8366af905c2a666d6e166e33f4
Mona <monaayman584@gmail.com>
2025-09-27 20:36:19 +0300

    refactor: modify mona to be robert

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Mona",
+                        name = "Robert",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
