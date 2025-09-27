ad70d6b72af69f84087393f2ae67f3c177076f53
Mona <monaayman584@gmail.com>
2025-09-27 19:31:48 +0300

    modify name

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Mona",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
